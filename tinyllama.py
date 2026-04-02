0ea8fae157b442fe72629dc8d44da987a4377243
asun975 <65915716+asun975@users.noreply.github.com>
2026-04-02 11:22:40 -0400

    create tinyllama.py

---
+++ tinyllama.py
@@ -0,0 +1,19 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# To use Transformers in an offline or firewalled environment requires the downloaded and cached files ahead of time. 
+tokenizer = AutoTokenizer.from_pretrained(r"models\huggingface\hub\models--TinyLlama--TinyLlama-1.1B-Chat-v1.0\snapshots\fe8a4ea1ffedaf415f4da2f062534de366a451e6", local_files_only=True)
+model = AutoModelForCausalLM.from_pretrained(r"models\huggingface\hub\models--TinyLlama--TinyLlama-1.1B-Chat-v1.0\snapshots\fe8a4ea1ffedaf415f4da2f062534de366a451e6", local_files_only=True)
+
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+inputs = tokenizer.apply_chat_template(
+	messages,
+	add_generation_prompt=True,
+	tokenize=True,
+	return_dict=True,
+	return_tensors="pt",
+).to(model.device)
+
+outputs = model.generate(**inputs, max_new_tokens=40)
+print(tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
