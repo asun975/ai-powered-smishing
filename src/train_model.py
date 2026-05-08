f628295edab656696a16fe866f7e7d6e7210705b
asun975 <65915716+asun975@users.noreply.github.com>
2026-05-08 17:20:48 -0400

    Change output folder and model save path

--- src/train_model.py
+++ src/train_model.py
@@ -1,11 +1,19 @@
 import pandas as pd
 from datasets import Dataset
+from os import getcwd, path
 from transformers import (
     AutoTokenizer,
     AutoModelForSequenceClassification,
     TrainingArguments,
     Trainer
 )
+
+BASE_DIR = getcwd()
+BASE_MODEL_DIR = path.join(BASE_DIR, 'models', 'sms-spam-model')
+NEW_MODEL_DIR = path.join(BASE_DIR, 'models', 'sms-spam-model-v2')
+
+print('Current working directory: ', BASE_DIR)
+print('Results will be saved to: ', NEW_MODEL_DIR)
 
 # -------------------------------
 # 1. LOAD NEW DATASET (WITH URLs)
@@ -29,7 +37,7 @@ dataset = dataset.train_test_split(test_size=0.2)
 # -------------------------------
 # 3. LOAD EXISTING MODEL
 # -------------------------------
-model_path = "./sms-spam-model"
+model_path = BASE_MODEL_DIR
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
@@ -57,11 +65,11 @@ dataset.set_format(
 # 5. TRAINING CONFIG
 # -------------------------------
 training_args = TrainingArguments(
-    output_dir="./sms_results_updated",
+    output_dir=path.join(NEW_MODEL_DIR, 'results'),
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
     num_train_epochs=2,  # small = avoids overfitting
-    logging_dir="./sms_logs_updated"
+    logging_dir=path.join(NEW_MODEL_DIR, 'logs')
 )
 
 # -------------------------------
@@ -82,7 +90,7 @@ trainer.train()
 # -------------------------------
 # 8. SAVE UPDATED MODEL
 # -------------------------------
-updated_model_path = "./sms-spam-model-v2"
+updated_model_path = NEW_MODEL_DIR
 
 trainer.save_model(updated_model_path)
 tokenizer.save_pretrained(updated_model_path)
