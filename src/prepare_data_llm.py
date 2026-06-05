09a58cfc614270cfe4822d1222127378c3623a5a
asun975 <65915716+asun975@users.noreply.github.com>
2026-06-05 09:54:26 -0400

    Remove batched testing small dataset size doesn't require batch testing

--- src/prepare_data_llm.py
+++ src/prepare_data_llm.py
@@ -1,17 +1,8 @@
-import numpy as np
 import os
 import pandas as pd
-import time
 import torch
 
 from datasets import Dataset
-from sklearn.metrics import (
-    accuracy_score, 
-    f1_score, 
-    recall_score, 
-    precision_score,
-    confusion_matrix
-)
 from transformers import (
     pipeline,
     AutoTokenizer,
@@ -20,12 +11,11 @@ from transformers import (
 
 from preprocessing import sanitize_text, removeUrl, text_preprocess, remove_special_char, maskPII
 
-N_SIZE=1000
-BATCH_SIZE=32
 MAX_LENGTH=128
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 MODEL_NAME = 'models/sms-spam-model-v2'
-DATASET_PATH = 'data/test_samples.csv'
+DATASET_PATH = 'sms test samples.csv'
+OUTPUT_PATH = 'data/llm_test_samples.csv'
 
 def load_model():
 
@@ -41,22 +31,6 @@ def load_model():
 
     return model, tokenizer
 
-def load_dataset(data, sample_size):
-    
-    print("Loading dataset...")
-    if not os.path.exists(DATASET_PATH):
-            raise FileNotFoundError(
-                f"Test samples not found at {DATASET_PATH}.\nRun: python src/prepare_data.py\nOr download test_samples.csv from https://github.com/asun975/ai-powered-smishing and save to data/"
-            )
-
-    # Create dataframe of n random samples
-    df = pd.read_csv(data)
-    df = df.sample(n=sample_size, random_state=42)
-    # Convert labels to numeric
-    df["label"] = df["label"].map({"ham": 0, "smishing": 1})
-    
-    return df
-
 def main():
     try:
         # Load model and tokenizer
@@ -65,8 +39,12 @@ def main():
         # HF text-classification
         classifier = pipeline("text-classification", device=DEVICE, model=model, tokenizer=tokenizer)
 
-        # Load dataset
-        df = load_dataset(DATASET_PATH, N_SIZE)
+        # Create dataframe of n random samples
+        df = pd.read_csv(DATASET_PATH)
+        df.drop(columns=['url'], inplace=True) # drop url column
+        df.rename(columns={'message':'text'}, inplace=True) # rename message col
+        # Convert labels to numeric
+        df["label"] = df["label"].map({"legitimate": 0, "phishing": 1})
 
         # Create column for sanitized sms text
         df["sanitized_text"] = df["text"].copy()
@@ -74,10 +52,6 @@ def main():
         df["sanitized_text"] = df["sanitized_text"].apply(lambda x: removeUrl(x))
         df["sanitized_text"] = df["sanitized_text"].apply(lambda x: text_preprocess(x))
         df["sanitized_text"] = df["sanitized_text"].apply(lambda x: remove_special_char(x))
-
-        # Create column for masked PII in sms text
-        df['masked_text'] = df['text'].copy()
-        df['masked_text'] = df['masked_text'].apply(lambda x: maskPII(x))
 
         # Convert to Hugging Face dataset
         dataset = Dataset.from_pandas(df)
@@ -92,33 +66,38 @@ def main():
                 return_tensors="pt")
             )
         
-        batched_dataset = dataset.batch(batch_size=BATCH_SIZE)
+        print(F"Running {MODEL_NAME}...")
+        texts = dataset['sanitized_text']
         all_results = []
-        all_preds = []
 
-        for batch in batched_dataset:
+        for t in texts:
+            result = classifier(t)[0]
+            pred = {"LABEL_0": 0, "LABEL_1": 1}[result["label"]]
+            score = result["score"]
+            risk_score = score * 100 if pred == "SPAM" else (1 - score) * 100
+            results = {
+                'processed_text': t, 
+                'pred': pred,
+                'risk_score': risk_score
+            }
+            all_results.append(results)
+    
+        # Convert results to dataframe
+        df_samples = pd.DataFrame.from_dict(all_results) 
 
-            for processed_text, original_text, masked_text, ground_truth in zip(batch['sanitized_text'], batch['text'], batch['masked_text'], batch['label']):
-                result = classifier(processed_text)[0]
-                pred = {"LABEL_0": 0, "LABEL_1": 1}[result["label"]]
-                score = result["score"]
-                risk_score = score * 100 if pred == "SPAM" else (1 - score) * 100
-                results = {
-                    'original_text': original_text,
-                    'processed_text': processed_text, 
-                    'masked_text': masked_text,
-                    'pred': pred,
-                    'label': ground_truth,
-                    'risk_score': risk_score
-                }
-                all_preds.append(pred)
-                all_results.append(results)
-       
-        # Save test results with original text
-        df_results = pd.DataFrame.from_dict(all_results)
-        # TODO: save original text and masked text for llm input
-        df_results.to_csv("data/test_results.csv")
-        print("Saved all results to data/test_results.csv")
+        df_samples['original_text'] = df['text'].copy() # Add back original text
+        print("Masking PII in sms input...")
+        # Create masked PII column that will pass to LLM
+        df_samples['masked_text'] = df['text'].copy()
+        df_samples['masked_text'] = df_samples['masked_text'].apply(lambda x: maskPII(x))
+
+        df_samples['label'] = df['label'].copy() # Add ground truth labels
+
+        print(df_samples.info()) # check dataframe contents
+
+        # Save to csv with index column
+        df_samples.to_csv(OUTPUT_PATH)
+        print(f"\nSaved all results to {OUTPUT_PATH}")
     
     except Exception as e:
             print(f"An unexpected exception occured of type {type(e)}")
