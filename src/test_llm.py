9d4c0dd628835071bafe9dc7c76a3a7f4d9ceb43
asun975 <65915716+asun975@users.noreply.github.com>
2026-06-05 17:20:43 -0400

    Create test_llm.py send a random sample from llm_test_samples.csv to Gemma2b model API prints model's response time

--- src/test_llm.py
+++ src/test_llm.py
@@ -1,34 +1,45 @@
 import requests
 import pandas as pd
+import json
+import time
 
 DATASET_PATH = 'data/llm_test_samples.csv'
+API_URL = '' # Paste endpoint url for LLM
 
-# Load test samples
-test_df = pd.read_csv(DATASET_PATH)
-test_df = test_df.reset_index()
-payload_df = test_df.loc[:, ['index', 'original_text', 'pred', 'risk_score']]
-payload_df = payload_df.rename(columns={'index':'id'})
-print(payload_df.info())
-"""
-# Base endpoint for API
-apiUrl = "/explain"
+if __name__ == "__main__":
+    # Load test samples
+    test_df = pd.read_csv(DATASET_PATH)
+    test_df = test_df.rename(columns={'pred':'classification', 'masked_text':'text'})
 
-headers = {
-    "Content-Type": "application/json",
-}
+    # Get one random sample 
+    sample = test_df.sample(n=1).iloc[0]
+    print(sample)
+   
+    # Data must match format expected by API
+    sample = sample[['text', 'classification', 'risk_score']]
+    payload = sample.to_dict()
 
-# Data must match json format expected by API
-payload = {
-    "sms_text":sms_text,
-    "classification": pred,
-    "risk_score": risk_score
-}
+    # Convert classification to string labels
+    payload['classification'] = "SPAM" if payload['classification'] == 1 else "SAFE"
+    print("dict: ", payload)
+    print("json.dumps(): ", json.dumps(payload))
 
-response = requests.post(apiUrl)
+    headers = {
+        "Content-Type": "application/json",
+    }
+    start_time = time.time()
+    response = requests.post(
+        url=API_URL, 
+        data=json.dumps(payload), 
+        headers=headers
+    )
 
-if response.status_code == 200:
-    data = response.json()
-    explanation = data['explanation']
-else:
-    error_detail = response.json()
-"""
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print(response.status_code)
+        print(response.headers.get("Content-Type"))
+        print(repr(response.text))
+    end_time = time.time()
+    print(f"Model response time: {end_time-start_time:.2f}")
