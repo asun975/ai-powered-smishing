bc4db3d100118e493e8a3ec9856c67f909a55306
GustavoDeVera <deverate@sheridancollege.ca>
2026-06-13 15:58:10 -0400

    Update build.gradle.kts

--- Local LLM/build.gradle.kts
+++ Local LLM/build.gradle.kts
@@ -59,4 +59,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 }
