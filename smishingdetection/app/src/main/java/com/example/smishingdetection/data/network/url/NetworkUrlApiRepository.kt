bb6033c832199c83dd25377fe1bab064798ae5af
asun975 <65915716+asun975@users.noreply.github.com>
2026-07-08 09:47:24 -0400

    Remove UrlApiSanitizer.kt move extract Url logic to repository class for url analyzer

--- smishingdetection/app/src/main/java/com/example/smishingdetection/data/network/url/NetworkUrlApiRepository.kt
+++ smishingdetection/app/src/main/java/com/example/smishingdetection/data/network/url/NetworkUrlApiRepository.kt
@@ -3,11 +3,12 @@ package com.example.smishingdetection.data.network.url
 import com.example.smishingdetection.data.network.url.model.UrlAnalyzerRequest
 import com.example.smishingdetection.data.network.url.model.UrlAnalyzerResponse
 
+// TODO add error data models for API responses
 /**
  * Repository that fetch url verdict from scanApi.
  */
 interface UrlApiRepository {
-    suspend fun getVerdict(request: UrlAnalyzerRequest): UrlAnalyzerResponse
+    suspend fun getVerdict(message: String): UrlAnalyzerResponse?
 }
 
 /**
@@ -16,5 +17,24 @@ interface UrlApiRepository {
 class NetworkUrlApiRepository(
     private val urlApiService: UrlApiService
 ) : UrlApiRepository {
-    override suspend fun getVerdict(request: UrlAnalyzerRequest): UrlAnalyzerResponse = urlApiService.getVerdict(request)
+
+    private fun extractUrl(body: String): List<String?> {
+        val urls = Regex("""(?i)\b((?:https?://|www\.)[^\s<>"']+)""").findAll(body)
+            .map { match ->
+                match.value.trimEnd(
+                    '.', ',', ';', ':', '!', '?', ')', ']', '}'
+                )
+            }
+            .toList()
+        return urls
+    }
+    override suspend fun getVerdict(message: String): UrlAnalyzerResponse? {
+        val urls = extractUrl(message)
+        val firstUrl = urls.getOrNull(0)
+        if (firstUrl != null) {
+            val request = UrlAnalyzerRequest(firstUrl)
+            return urlApiService.getVerdict(request)
+        }
+        return null
+    }
 }
